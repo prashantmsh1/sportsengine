a869f8acdd937e958c3c7a3b81907442c8de47c5
Prashant <prashantkr.msh@gmail.com>
2026-02-18 19:19:00 +0530

    refactor: rebase Drizzle migrations to consolidate `matches` and `commentary` tables into a new initial migration.

--- packages/database/drizzle/0000_grey_moonstone.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text,
-	"email" text,
-	"created_at" timestamp DEFAULT now(),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
